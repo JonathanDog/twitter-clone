9d003f57ee0c9c746b33275215dcd1cfc75b4894
JonathanDog <46735912+JonathanDog@users.noreply.github.com>
2022-07-28 17:09:48 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 
 const clientCredentials = {
-    apiKey: "AIzaSyDqOTGLPPzG_wEgzoOaZr9GSzl1ToVQw6w",
-    authDomain: "websockets-ae419.firebaseapp.com",
-    projectId: "websockets-ae419",
-    storageBucket: "websockets-ae419.appspot.com",
-    messagingSenderId: "444438905263",
-    appId: "1:444438905263:web:3ae971ca338adca3732f5a"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 }
 let app; 
 try {
